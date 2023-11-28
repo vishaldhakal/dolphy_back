8197b9d55b583c14e0765b086bedc42623bcf75e
vishaldhakal <vishaldhakal96@gmail.com>
2023-11-28 22:00:41 +0545

    sdV'

--- preconstruction/urls.py
+++ preconstruction/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from .views import DeveloperListCreateView,PreConstructionsDeveloper,news_detail, slugify_all_news,DeveloperRetrieveUpdateDeleteView, PreConstructionListCreateView, PreConstructionRetrieveUpdateDeleteView, EventListCreateView, EventRetrieveUpdateDeleteView, NewsListCreateView, NewsRetrieveUpdateDeleteView, FavouriteListCreateView, FavouriteRetrieveUpdateDeleteView, CityListCreateView, CityRetrieveUpdateDeleteView, PreConstructionDetailView, PreConstructionsCityView, delete_image, delete_floorplan, remove_last_part_of_slug, get_all_city, get_related_precons,get_all_precons
+from .views import DeveloperListCreateView,ContactFormSubmission,PreConstructionsDeveloper,news_detail, slugify_all_news,DeveloperRetrieveUpdateDeleteView, PreConstructionListCreateView, PreConstructionRetrieveUpdateDeleteView, EventListCreateView, EventRetrieveUpdateDeleteView, NewsListCreateView, NewsRetrieveUpdateDeleteView, FavouriteListCreateView, FavouriteRetrieveUpdateDeleteView, CityListCreateView, CityRetrieveUpdateDeleteView, PreConstructionDetailView, PreConstructionsCityView, delete_image, delete_floorplan, remove_last_part_of_slug, get_all_city, get_related_precons,get_all_precons
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -50,6 +50,7 @@ urlpatterns = [
     path('news/<str:slug>/', news_detail,
          name='news-detail'),
     path('city/', CityListCreateView.as_view(), name='city-list-create'),
+    path('contact-form-submission/', ContactFormSubmission, name='contact-form-submission'),
     path('all-city/', get_all_city, name='get-all-city'),
     path('all-precons/', get_all_precons, name='get-all-precons'),
     path('city/<int:pk>/', CityRetrieveUpdateDeleteView.as_view(),
