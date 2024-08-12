65051238514da620edb6dc1d5b6796bac6015f49
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-12 11:12:22 +0545

    dfv

--- preconstruction/views.py
+++ preconstruction/views.py
@@ -513,14 +513,14 @@ def ContactFormSubmission(request):
         if validate_name(request.POST["name"]) and validate_email(request.POST["email"]) and validate_phone(request.POST["phone"]):
             body = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}\nIs a realtor?: {realtor}"
             email = EmailMessage(
-                subject, body, emaill, ["hello@dolphy.ca"],
+                subject, body, emaill, ["hello@dolphy.ca","contact@homebaba.ca"],
                 reply_to=[email], headers=headers
             )
             email.send(fail_silently=False)
             return HttpResponse("Sucess")
         else:
             email = EmailMessage(
-                subject, body, emaill, ["hello@dolphy.ca"],
+                subject, body, emaill, ["hello@dolphy.ca","contact@homebaba.ca"],
                 reply_to=[email], headers=headers
             )
             email.send(fail_silently=False)
