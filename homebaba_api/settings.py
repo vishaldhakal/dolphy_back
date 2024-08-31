7e083a2a19d5077c852628940c0129d9a4f582ed
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-31 23:49:49 +0545

    Sdv

--- homebaba_api/settings.py
+++ homebaba_api/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -204,7 +207,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.resend.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'resend'
-EMAIL_HOST_PASSWORD = 're_eY6eeqwr_FRPYbQd3rtdPUXUHDVKS5H7Y'
+EMAIL_HOST_PASSWORD = os.getenv("RESEND_APIKEY", default="")
 
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
